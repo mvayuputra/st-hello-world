eb60db8ad836ea2c14d51b1e8faa24d823f7bfc2
mvayuputra <115626029+mvayuputra@users.noreply.github.com>
2023-08-08 19:11:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,11 +6,9 @@ with st.form("my_form"):
     option = st.selectbox(
     'How would you like to be contacted?',
     ('Email', 'Home phone', 'Mobile phone'))
-
-
-   # Every form must have a submit button.
-   submitted = st.form_submit_button("Submit")
-   if submitted:
+    # Every form must have a submit button.
+    submitted = st.form_submit_button("Submit")
+    if submitted:
        st.write("slider", optiom)
 
 st.write("Outside the form ",option)
