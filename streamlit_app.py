2f67f43acb47f3828373980dd0ff4821da893d96
mvayuputra <115626029+mvayuputra@users.noreply.github.com>
2023-08-08 19:22:19 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,14 +1,23 @@
 import streamlit as st
 st.write("PRINCE EDUCATION")
-number = st.slider("Pick a number", 0, 100)
-st.write(number+5)
 with st.form("my_form"):
     option = st.selectbox(
-    'How would you like to be contacted?',
-    ('Email', 'Home phone', 'Mobile phone'))
+    'prince profession',
+    ('killer', 'hunter', 'none of the above'))
+
+    option_two = st.radio(
+        'prince character',
+        ['good','worse','neutral'])
+    scale = st.slider("rating for prince",0,10)
+    
     # Every form must have a submit button.
     submitted = st.form_submit_button("Submit")
     if submitted:
-       st.write("slider", option)
+        if option_two == 'good' and option == 'killer':
+            st.write('you won')
+            st.write('score : ',scale*10)
+        else:
+            st.write('you lost')
+        st.write("slider", option)
 
-st.write("Outside the form ",option)
+st.write("reload for another game ")
