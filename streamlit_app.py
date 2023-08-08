504590e6388e53317f7ed3373f5a9dfeb5374002
mvayuputra <115626029+mvayuputra@users.noreply.github.com>
2023-08-08 19:10:07 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,3 +2,15 @@ import streamlit as st
 st.write("PRINCE EDUCATION")
 number = st.slider("Pick a number", 0, 100)
 st.write(number+5)
+with st.form("my_form"):
+  option = st.selectbox(
+    'How would you like to be contacted?',
+    ('Email', 'Home phone', 'Mobile phone'))
+
+
+   # Every form must have a submit button.
+   submitted = st.form_submit_button("Submit")
+   if submitted:
+       st.write("slider", optiom)
+
+st.write("Outside the form ",option)
